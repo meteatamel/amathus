f11edb884cb96ff0fa607c310bc02234964aac5a
deniz.vehbi@gmail.com <deniz.vehbi@gmail.com>
2020-10-11 22:31:55 +0100

    additional files

---
+++ AmathusClient/lib/ad_manager.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+class AdManager {
+
+  static String get appId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544~4354546703";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-8735582123402927~8560233916";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/8865242552";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-8735582123402927/8149509686";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/7049598008";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/3964253750";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/8673189370";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/7552160883";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+}
