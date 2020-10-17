0e595c1570811bc31bbec239c43abcbbe861a261
Mete Atamel <atamel@google.com>
2020-10-17 17:18:08 +0100

    Finished news source ordering

--- AmathusClient/lib/controllers/feeds_controller.dart
+++ AmathusClient/lib/controllers/feeds_controller.dart
@@ -16,27 +16,55 @@ class FeedsController {
   }
 
   Future<List<Feed>> read() async {
-    var feeds;
+    final storedFeeds = await readFromStorage();
 
     try {
       final response = await http.get(Constants.URL_FEEDS);
       if (response.statusCode == 200) {
-        feeds = (json.decode(response.body) as List).map((i) =>
+        final receivedFeeds = (json.decode(response.body) as List).map((i) =>
             Feed.fromJson(i)).toList();
-        _storage.write(feeds);
+        final orderedFeeds = await orderAndStoreFeeds(storedFeeds, receivedFeeds);
+        return orderedFeeds;
       }
-      feeds = await readFromStorage();
-    } on SocketException catch (_) {
-      feeds = await readFromStorage();
+    } on SocketException catch (e) {
+      // Just return the stored feeds
     }
-    return feeds;
+
+    return storedFeeds;
   }
 
   Future<List<Feed>> readFromStorage() async {
-    var feeds = _storage.read();
+    var feeds = await _storage.read();
     if (feeds == null) {
       throw Exception('Failed to load feeds');
     }
     return feeds;
+  }
+
+  Future<List<Feed>> orderAndStoreFeeds(List<Feed> storedFeeds, List<Feed> receivedFeeds) async {
+    if (receivedFeeds == null || receivedFeeds.isEmpty) {
+      return storedFeeds;
+    }
+
+    if (storedFeeds == null || storedFeeds.isEmpty) {
+      await _storage.write(receivedFeeds);
+      return receivedFeeds;
+    }
+
+    var orderedFeeds = new List<Feed>();
+
+    for (var i = 0; i < storedFeeds.length; i++) {
+      var storedFeed = storedFeeds[i];
+      var index = receivedFeeds.indexWhere((element) => element.id == storedFeed.id);
+      if (index != -1) {
+        var receivedFeed = receivedFeeds.removeAt(index);
+        orderedFeeds.add(receivedFeed);
+      }
+    }
+
+    orderedFeeds.addAll(receivedFeeds);
+    await _storage.write(orderedFeeds);
+
+    return orderedFeeds;
   }
 }
